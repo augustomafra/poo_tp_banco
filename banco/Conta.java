b861784282fc00ac92313780c4fa32843045fe62
augusto <augustomafra1@gmail.com>
2018-05-25 16:48:51 -0300

    Adicionando metodos na classe Conta

--- banco/Conta.java
+++ banco/Conta.java
@@ -11,12 +11,64 @@
 package banco;
 
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 
 class Conta {
     private int numConta;
     private double saldo;
     private Cliente cliente;
     private ArrayList<banco.Movimentacao> movimentacoes;
-    // TODO Descobrir para que proximoNumConta serve
-    static private int proximoNumConta;
+    static private int proximoNumConta = 0;
+
+    public Conta(Cliente cliente){
+        numConta = proximoNumConta++;
+        saldo = 0;
+        this.cliente = cliente;
+    }
+
+    public int getNumConta(){
+        return numConta;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public void creditar(double valor, String descricao){
+        saldo += valor;
+        movimentacoes.add(new Movimentacao(descricao, 'C', valor));
+    }
+
+    public void debitar(double valor, String descricao){
+        if (valor > saldo) return;
+        saldo -= valor;
+        movimentacoes.add(new Movimentacao(descricao, 'D', valor));
+    }
+
+    public ArrayList<banco.Movimentacao> getExtrato(GregorianCalendar dataInicial, GregorianCalendar dataFinal){
+        ArrayList<banco.Movimentacao> extrato = new ArrayList<banco.Movimentacao>();
+        for (banco.Movimentacao mov : movimentacoes){
+            GregorianCalendar data = mov.getDataMov();
+            if ((data.after(dataInicial) &&
+                 data.before(dataFinal)) ||
+                 data.compareTo(dataInicial) == 0 ||
+                 data.compareTo(dataFinal) == 0){
+                extrato.add(mov);
+            }
+        }
+        return extrato;
+    }
+
+    public ArrayList<banco.Movimentacao> getExtrato(GregorianCalendar dataInicial){
+        GregorianCalendar dataFinal = new GregorianCalendar(); // data atual
+        return getExtrato(dataInicial, dataFinal);
+    }
+
+    public ArrayList<banco.Movimentacao> getExtrato(){
+        GregorianCalendar dataInicial = new GregorianCalendar();
+        GregorianCalendar dataFinal = new GregorianCalendar();
+        dataInicial.set(GregorianCalendar.DAY_OF_MONTH, 1);
+        dataFinal.set(GregorianCalendar.DAY_OF_MONTH, dataFinal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH));
+        return getExtrato(dataInicial, dataFinal);
+    }
 }
