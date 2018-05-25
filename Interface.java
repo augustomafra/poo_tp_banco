fad07c6f38a60f9b84d2549a42e4772aef70b476
augusto <augustomafra1@gmail.com>
2018-05-25 15:31:50 -0300

    Organizando estrutura das funcoes para executar comandos na Interface

--- Interface.java
+++ Interface.java
@@ -12,16 +12,18 @@ import java.util.Scanner;
 import banco.Banco;
 
 public class Interface {
-    private static void iniciarLinhaDeComando() {
-        scan = new Scanner(System.in);
 
-        System.out.println("Universidade Federal de Minas Gerais");
-        System.out.println("Programacao Orientada a Objetos");
-        System.out.println("2018-1\n");
-        System.out.println("Trabalho Pratico 1");
-        System.out.println("Sistema de Gerenciamento de Banco\n");
-        System.out.println("Andre Lage");
-        System.out.println("Augusto Mafra\n");
+/************************* Criar Comandos aqui dentro *************************/
+    // Recohnece uma string como sendo um comando listado no enum 'Comando'
+    // Se a string nao corresponder a nenhum item do enum, retorna o enum ERRO
+    private static Comando reconhecerComando(String cmd_string) {
+        if (cmd_string.equals("ajuda")) {
+            return Comando.AJUDA;
+        } else if (cmd_string.equals("sair")) {
+            return Comando.SAIR;
+        } else {
+            return Comando.ERRO;
+        }
     }
 
     private static Comando executarLinhaDeComando() {
@@ -38,22 +40,24 @@ public class Interface {
         return comando;
     }
 
-    // Recohnece uma string como sendo um comando listado no enum 'Comando'
-    // Se a string nao corresponder a nenhum item do enum, retorna o enum ERRO
-    private static Comando reconhecerComando(String cmd_string) {
-        if (cmd_string.equals("ajuda")) {
-            return Comando.AJUDA;
-        } else if (cmd_string.equals("sair")) {
-            return Comando.SAIR;
-        } else {
-            return Comando.ERRO;
-        }
-    }
-
+    // Funcoes auxiliares para os comandos
     private static void mostrarListaDeComandos() {
         System.out.println("Lista de comandos:");
         System.out.println("ajuda");
         System.out.println("sair");
+    }
+/************************* Criar Comandos aqui dentro *************************/
+
+    private static void iniciarLinhaDeComando() {
+        scan = new Scanner(System.in);
+
+        System.out.println("Universidade Federal de Minas Gerais");
+        System.out.println("Programacao Orientada a Objetos");
+        System.out.println("2018-1\n");
+        System.out.println("Trabalho Pratico 1");
+        System.out.println("Sistema de Gerenciamento de Banco\n");
+        System.out.println("Andre Lage");
+        System.out.println("Augusto Mafra\n");
     }
 
     public static void main(String[] args) {
